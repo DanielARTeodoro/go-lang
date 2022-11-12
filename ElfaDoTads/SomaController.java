7615489a5b2345bfb2d8a34390d3e2dfe87f946d
Dani <51429507+DanielARTeodoro@users.noreply.github.com>
2022-11-11 21:44:10 -0300

    Create SomaController.java
    SomaController

---
+++ ElfaDoTads/SomaController.java
@@ -0,0 +1,21 @@
+package br.edu.ifms.sweet.controller;
+
+import br.edu.ifms.sweet.exception.NoNumeroException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sum")
+public class SomaController {
+
+    @GetMapping(value = "/{num1}/{num2}")
+    public Integer getSum(@PathVariable String num1, @PathVariable String num2){
+         if(num1.matches("[0-9]+") && num2.matches("[0-9]+")){
+             return Integer.parseInt(num1) + Integer.parseInt(num2);
+         } else {
+             throw new NoNumeroException("Não pode usar letras em Soma!");
+         }
+    }
+}
