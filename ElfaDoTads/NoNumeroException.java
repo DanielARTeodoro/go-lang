15967cd3a1f33e9769d902757c684e7c4ac50575
Dani <51429507+DanielARTeodoro@users.noreply.github.com>
2022-11-11 21:13:22 -0300

    Create NoNumeroException.java
    NoNumeroException

---
+++ ElfaDoTads/NoNumeroException.java
@@ -0,0 +1,8 @@
+package br.edu.ifms.sweet.exception;
+
+public class NoNumeroException extends RuntimeException{
+
+    public NoNumeroException (String mensagem) {
+        super(mensagem);
+    }
+}
