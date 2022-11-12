1024cef227512a090e56859955c94b094918729f
Dani <51429507+DanielARTeodoro@users.noreply.github.com>
2022-11-11 21:39:29 -0300

    Create CustomizedResponseEntityExceptionHandler.java
    CustomizedResponseEntityExceptionHandler

---
+++ ElfaDoTads/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,30 @@
+package br.edu.ifms.sweet.exception.handler;
+
+import br.edu.ifms.sweet.exception.ExceptionResponse;
+import br.edu.ifms.sweet.exception.NoNumeroException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@RestController
+@ControllerAdvice // Captura as Exceções
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex, WebRequest request) {
+        var exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(NoNumeroException.class)
+    public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception ex, WebRequest request) {
+        var exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+}
