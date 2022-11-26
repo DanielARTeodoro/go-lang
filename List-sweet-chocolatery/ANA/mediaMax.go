02a89feba013bc816a04718ec24a1b24dd648b52
Dani <51429507+DanielARTeodoro@users.noreply.github.com>
2022-11-26 17:11:06 -0300

    ANA 7 - mediaMax - Go Lang
    ANA 7 - Crie uma função que receba 10 números, faça a média e mostre o maior número

---
+++ List-sweet-chocolatery/ANA/mediaMax.go
@@ -0,0 +1,765 @@
+package main
+
+import "fmt"
+
+/// ANA 7 - Crie uma função que receba 10 números, faça a média e mostre o maior número
+
+var (
+	n     float64
+	n0    float64
+	n1    float64
+	n2    float64
+	n3    float64
+	n4    float64
+	n5    float64
+	n6    float64
+	n7    float64
+	n8    float64
+	media float64
+)
+
+func mediaMax() float64 {
+
+	fmt.Println("Digite 10 valores: ")
+	fmt.Println("Digite o valor 1: ")
+	fmt.Scan(&n)
+	fmt.Println("Digite o valor 2: ")
+	fmt.Scan(&n0)
+	fmt.Println("Digite o valor 3: ")
+	fmt.Scan(&n1)
+	fmt.Println("Digite o valor 4: ")
+	fmt.Scan(&n2)
+	fmt.Println("Digite o valor 5: ")
+	fmt.Scan(&n3)
+	fmt.Println("Digite o valor 6: ")
+	fmt.Scan(&n4)
+	fmt.Println("Digite o valor 7: ")
+	fmt.Scan(&n5)
+	fmt.Println("Digite o valor 8: ")
+	fmt.Scan(&n6)
+	fmt.Println("Digite o valor 9: ")
+	fmt.Scan(&n7)
+	fmt.Println("Digite o valor 10: ")
+	fmt.Scan(&n8)
+	media = n + n0 + n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8
+	fmt.Println("A media é: ", media/10)
+
+	if n0 < n {
+		fmt.Println("O maior numero é: ", n)
+		return n
+
+	}
+	if n < n0 {
+		fmt.Println("O maior numero é: ", n0)
+
+		return n0
+	}
+	if n < n1 {
+
+		fmt.Println("O maior numero é: ", n1)
+		return n1
+	}
+	if n < n2 {
+
+		fmt.Println("O maior numero é: ", n2)
+		return n2
+	}
+	if n < n3 {
+
+		fmt.Println("O maior numero é: ", n3)
+		return n3
+	}
+	if n < n4 {
+
+		fmt.Println("O maior numero é: ", n4)
+		return n4
+	}
+	if n < n5 {
+
+		fmt.Println("O maior numero é: ", n5)
+		return n5
+
+	}
+	if n < n6 {
+
+		fmt.Println("O maior numero é: ", n6)
+		return n6
+	}
+	if n < n7 {
+
+		fmt.Println("O maior numero é: ", n7)
+		return n7
+	}
+	if n < n8 {
+
+		fmt.Println("O maior numero é: ", n8)
+		return n8
+	}
+
+	/// ======================
+
+	if n1 < n {
+		fmt.Println("O maior numero é: ", n)
+		return n
+
+	}
+	if n1 < n0 {
+		fmt.Println("O maior numero é: ", n0)
+
+		return n0
+	}
+	if n0 < n1 {
+
+		fmt.Println("O maior numero é: ", n1)
+		return n1
+	}
+	if n1 < n2 {
+
+		fmt.Println("O maior numero é: ", n2)
+		return n2
+	}
+	if n1 < n3 {
+
+		fmt.Println("O maior numero é: ", n3)
+		return n3
+	}
+	if n1 < n4 {
+
+		fmt.Println("O maior numero é: ", n4)
+		return n4
+	}
+	if n1 < n5 {
+
+		fmt.Println("O maior numero é: ", n5)
+		return n5
+
+	}
+	if n1 < n6 {
+
+		fmt.Println("O maior numero é: ", n6)
+		return n6
+	}
+	if n1 < n7 {
+
+		fmt.Println("O maior numero é: ", n7)
+		return n7
+	}
+	if n1 < n8 {
+
+		fmt.Println("O maior numero é: ", n8)
+		return n8
+	}
+
+	/// ==================
+
+	if n2 < n {
+		fmt.Println("O maior numero é: ", n)
+		return n
+
+	}
+	if n2 < n0 {
+		fmt.Println("O maior numero é: ", n0)
+
+		return n0
+	}
+	if n2 < n1 {
+
+		fmt.Println("O maior numero é: ", n1)
+		return n1
+	}
+	if n0 < n2 {
+
+		fmt.Println("O maior numero é: ", n2)
+		return n2
+	}
+	if n2 < n3 {
+
+		fmt.Println("O maior numero é: ", n3)
+		return n3
+	}
+	if n2 < n4 {
+
+		fmt.Println("O maior numero é: ", n4)
+		return n4
+	}
+	if n2 < n5 {
+
+		fmt.Println("O maior numero é: ", n5)
+		return n5
+
+	}
+	if n2 < n6 {
+
+		fmt.Println("O maior numero é: ", n6)
+		return n6
+	}
+	if n2 < n7 {
+
+		fmt.Println("O maior numero é: ", n7)
+		return n7
+	}
+	if n2 < n8 {
+
+		fmt.Println("O maior numero é: ", n8)
+		return n8
+	}
+
+	/// =================
+
+	if n3 < n {
+		fmt.Println("O maior numero é: ", n)
+		return n
+
+	}
+	if n3 < n0 {
+		fmt.Println("O maior numero é: ", n0)
+
+		return n0
+	}
+	if n3 < n1 {
+
+		fmt.Println("O maior numero é: ", n1)
+		return n1
+	}
+	if n3 < n2 {
+
+		fmt.Println("O maior numero é: ", n2)
+		return n2
+	}
+	if n0 < n3 {
+
+		fmt.Println("O maior numero é: ", n3)
+		return n3
+	}
+	if n3 < n4 {
+
+		fmt.Println("O maior numero é: ", n4)
+		return n4
+	}
+	if n3 < n5 {
+
+		fmt.Println("O maior numero é: ", n5)
+		return n5
+
+	}
+	if n3 < n6 {
+
+		fmt.Println("O maior numero é: ", n6)
+		return n6
+	}
+	if n3 < n7 {
+
+		fmt.Println("O maior numero é: ", n7)
+		return n7
+	}
+	if n3 < n8 {
+
+		fmt.Println("O maior numero é: ", n8)
+		return n8
+	}
+
+	/// ======================
+
+	if n4 < n {
+		fmt.Println("O maior numero é: ", n)
+		return n
+
+	}
+	if n4 < n0 {
+		fmt.Println("O maior numero é: ", n0)
+
+		return n0
+	}
+	if n4 < n1 {
+
+		fmt.Println("O maior numero é: ", n1)
+		return n1
+	}
+	if n4 < n2 {
+
+		fmt.Println("O maior numero é: ", n2)
+		return n2
+	}
+	if n4 < n3 {
+
+		fmt.Println("O maior numero é: ", n3)
+		return n3
+	}
+	if n0 < n4 {
+
+		fmt.Println("O maior numero é: ", n4)
+		return n4
+	}
+	if n4 < n5 {
+
+		fmt.Println("O maior numero é: ", n5)
+		return n5
+
+	}
+	if n4 < n6 {
+
+		fmt.Println("O maior numero é: ", n6)
+		return n6
+	}
+	if n4 < n7 {
+
+		fmt.Println("O maior numero é: ", n7)
+		return n7
+	}
+	if n4 < n8 {
+
+		fmt.Println("O maior numero é: ", n8)
+		return n8
+	}
+
+	/// ==================
+
+	if n5 < n {
+		fmt.Println("O maior numero é: ", n)
+		return n
+
+	}
+	if n5 < n0 {
+		fmt.Println("O maior numero é: ", n0)
+
+		return n0
+	}
+	if n5 < n1 {
+
+		fmt.Println("O maior numero é: ", n1)
+		return n1
+	}
+	if n5 < n2 {
+
+		fmt.Println("O maior numero é: ", n2)
+		return n2
+	}
+	if n5 < n3 {
+
+		fmt.Println("O maior numero é: ", n3)
+		return n3
+	}
+	if n5 < n4 {
+
+		fmt.Println("O maior numero é: ", n4)
+		return n4
+	}
+	if n0 < n5 {
+
+		fmt.Println("O maior numero é: ", n5)
+		return n5
+
+	}
+	if n5 < n6 {
+
+		fmt.Println("O maior numero é: ", n6)
+		return n6
+	}
+	if n5 < n7 {
+
+		fmt.Println("O maior numero é: ", n7)
+		return n7
+	}
+	if n5 < n8 {
+
+		fmt.Println("O maior numero é: ", n8)
+		return n8
+	}
+
+	/// ===============
+
+	if n6 < n {
+		fmt.Println("O maior numero é: ", n)
+		return n
+
+	}
+	if n6 < n0 {
+		fmt.Println("O maior numero é: ", n0)
+
+		return n0
+	}
+	if n6 < n1 {
+
+		fmt.Println("O maior numero é: ", n1)
+		return n1
+	}
+	if n6 < n2 {
+
+		fmt.Println("O maior numero é: ", n2)
+		return n2
+	}
+	if n6 < n3 {
+
+		fmt.Println("O maior numero é: ", n3)
+		return n3
+	}
+	if n6 < n4 {
+
+		fmt.Println("O maior numero é: ", n4)
+		return n4
+	}
+	if n6 < n5 {
+
+		fmt.Println("O maior numero é: ", n5)
+		return n5
+
+	}
+	if n0 < n6 {
+
+		fmt.Println("O maior numero é: ", n6)
+		return n6
+	}
+	if n6 < n7 {
+
+		fmt.Println("O maior numero é: ", n7)
+		return n7
+	}
+	if n6 < n8 {
+
+		fmt.Println("O maior numero é: ", n8)
+		return n8
+	}
+
+	/// ======================
+
+	if n7 < n {
+		fmt.Println("O maior numero é: ", n)
+		return n
+
+	}
+	if n7 < n0 {
+		fmt.Println("O maior numero é: ", n0)
+
+		return n0
+	}
+	if n7 < n1 {
+
+		fmt.Println("O maior numero é: ", n1)
+		return n1
+	}
+	if n7 < n2 {
+
+		fmt.Println("O maior numero é: ", n2)
+		return n2
+	}
+	if n7 < n3 {
+
+		fmt.Println("O maior numero é: ", n3)
+		return n3
+	}
+	if n7 < n4 {
+
+		fmt.Println("O maior numero é: ", n4)
+		return n4
+	}
+	if n7 < n5 {
+
+		fmt.Println("O maior numero é: ", n5)
+		return n5
+
+	}
+	if n7 < n6 {
+
+		fmt.Println("O maior numero é: ", n6)
+		return n6
+	}
+	if n0 < n7 {
+
+		fmt.Println("O maior numero é: ", n7)
+		return n7
+	}
+	if n7 < n8 {
+
+		fmt.Println("O maior numero é: ", n8)
+		return n8
+	}
+
+	/// ==================
+
+	if n8 < n {
+		fmt.Println("O maior numero é: ", n)
+		return n
+
+	}
+	if n8 < n0 {
+		fmt.Println("O maior numero é: ", n0)
+
+		return n0
+	}
+	if n8 < n1 {
+
+		fmt.Println("O maior numero é: ", n1)
+		return n1
+	}
+	if n8 < n2 {
+
+		fmt.Println("O maior numero é: ", n2)
+		return n2
+	}
+	if n8 < n3 {
+
+		fmt.Println("O maior numero é: ", n3)
+		return n3
+	}
+	if n8 < n4 {
+
+		fmt.Println("O maior numero é: ", n4)
+		return n4
+	}
+	if n8 < n5 {
+
+		fmt.Println("O maior numero é: ", n5)
+		return n5
+
+	}
+	if n8 < n6 {
+
+		fmt.Println("O maior numero é: ", n6)
+		return n6
+	}
+	if n8 < n7 {
+
+		fmt.Println("O maior numero é: ", n7)
+		return n7
+	}
+	if n0 < n8 {
+
+		fmt.Println("O maior numero é: ", n8)
+		return n8
+	}
+
+	/// ===============
+
+	{
+		return 0
+	}
+}
+
+// /==================================
+//func MaxN(n, n0, n1, n2, n3, n4, n5, n6, n7, n8 float64) float64 {
+//	if x >= n {
+//		fmt.Printf("O maior numero é: %f", n)
+//		return n
+//
+//	}
+//	if x <= n0 {
+//		return n0
+//		fmt.Println("O maior numero é: ", n0)
+//	}
+//	if x <= n1 {
+//		return n1
+//		fmt.Println("O maior numero é: ", n1)
+//	}
+//	if x <= n2 {
+//		return n2
+//		fmt.Println("O maior numero é: ", n2)
+//	}
+//	if x <= n3 {
+//		return n3
+//		fmt.Println("O maior numero é: ", n3)
+//	}
+//	if x <= n4 {
+//		return n4
+//		fmt.Println("O maior numero é: ", n4)
+//	}
+//	if x <= n5 {
+//		return n5
+//		fmt.Println("O maior numero é: ", n5)
+//	}
+//	if x <= n6 {
+//		return n6
+//		fmt.Println("O maior numero é: ", n6)
+//	}
+//	if x <= n7 {
+//		return n7
+//		fmt.Println("O maior numero é: ", n7)
+//	}
+//	if x <= n8 {
+//		return n8
+//		fmt.Println("O maior numero é: ", n8)
+//	}
+//	{
+//		return x
+//	}
+//}
+
+// Max
+//
+//	func Max(n, n0, n1, n2, n3, n4, n5, n6, n7, n8 float64) float64 {
+//		return 0
+//	}
+func main() {
+	mediaMax()
+
+	//var (
+	//	n0  float64 = n
+	//	n00 float64 = n0
+	//	n10 float64 = n1
+	//	n20 float64 = n2
+	//	n30 float64 = n3
+	//	n40 float64 = n4
+	//	n50 float64 = n5
+	//	n60 float64 = n6
+	//	n70 float64 = n7
+	//	n80 float64 = n8
+	//)
+
+	///MaxN(n, n0, n1, n2, n3, n4, n5, n6, n7, n8)
+	//
+	//var resultx float64 = math.Max(n, n0)
+	//var result0 float64 = math.Max(n, n1)
+	//var result1 float64 = math.Max(n, n2)
+	//var result2 float64 = math.Max(n, n3)
+	//var result3 float64 = math.Max(n, n4)
+	//var result4 float64 = math.Max(n, n5)
+	//var result5 float64 = math.Max(n, n6)
+	//var result6 float64 = math.Max(n, n7)
+	//var result7 float64 = math.Max(n, n8)
+	//
+	//var a float64 = math.Max(resultx, result0)
+	//var b float64 = math.Max(result4, result1)
+	//var c float64 = math.Max(result5, result2)
+	//var d float64 = math.Max(result6, result3)
+	//
+	//var aa float64 = math.Max(a, b)
+	//var bb float64 = math.Max(c, d)
+	//var cc float64 = math.Max(aa, bb)
+	//var dd float64 = math.Max(result7, cc)
+	//
+	//fmt.Printf("Result 1: %f", dd)
+	//
+	//////===============
+	//var resultn float64 = math.Max(n1, n0)
+	//var result0n float64 = math.Max(n1, n)
+	//var result1n float64 = math.Max(n1, n2)
+	//var result2n float64 = math.Max(n1, n3)
+	//var result3n float64 = math.Max(n1, n4)
+	//var result4n float64 = math.Max(n1, n5)
+	//var result5n float64 = math.Max(n1, n6)
+	//var result6n float64 = math.Max(n1, n7)
+	//var result7n float64 = math.Max(n1, n8)
+	//
+	//var a1 float64 = math.Max(resultn, result0n)
+	//var b1 float64 = math.Max(result4n, result1n)
+	//var c1 float64 = math.Max(result5n, result2n)
+	//var d1 float64 = math.Max(result6n, result3n)
+	//
+	//var aa1 float64 = math.Max(a1, b1)
+	//var bb1 float64 = math.Max(c1, d1)
+	//var cc1 float64 = math.Max(aa1, bb1)
+	//var dd1 float64 = math.Max(result7n, cc1)
+	//
+	//fmt.Printf("Result 1: %f", dd1)
+	//
+	/////===============
+	//var resultnn float64 = math.Max(n0, n)
+	//var result0nn float64 = math.Max(n0, n1)
+	//var result1nn float64 = math.Max(n0, n2)
+	//var result2nn float64 = math.Max(n0, n3)
+	//var result3nn float64 = math.Max(n0, n4)
+	//var result4nn float64 = math.Max(n0, n5)
+	//var result5nn float64 = math.Max(n0, n6)
+	//var result6nn float64 = math.Max(n0, n7)
+	//var result7nn float64 = math.Max(n0, n8)
+	//
+	//var a0 float64 = math.Max(resultnn, result0nn)
+	//var b0 float64 = math.Max(result4nn, result1nn)
+	//var c0 float64 = math.Max(result5nn, result2nn)
+	//var d0 float64 = math.Max(result6nn, result3nn)
+	//
+	//var aa0 float64 = math.Max(a0, b0)
+	//var bb0 float64 = math.Max(c0, d0)
+	//var cc0 float64 = math.Max(aa0, bb0)
+	//var dd0 float64 = math.Max(result7nn, cc0)
+	//
+	//fmt.Printf("Result 1: %f", dd0)
+	//
+	//var t float64 = math.Max(dd, dd0)
+	//var t0 float64 = math.Max(dd0, dd1)
+	//var t1 float64 = math.Max(dd1, dd)
+	//
+	//var tt float64 = math.Max(t, t0)
+	//var tt0 float64 = math.Max(t0, t1)
+	//var result float64 = math.Max(tt, tt0)
+
+	///============
+	//var resultnnn float64 = math.Max(n2, n0)
+	//var result0nnn float64 = math.Max(n2, n1)
+	//var result1nnn float64 = math.Max(n2, n)
+	//var result2nnn float64 = math.Max(n2, n3)
+	//var result3nnn float64 = math.Max(n2, n4)
+	//var result4nnn float64 = math.Max(n2, n5)
+	//var result5nnn float64 = math.Max(n2, n6)
+	//var result6nnn float64 = math.Max(n2, n7)
+	//var result7nnn float64 = math.Max(n2, n8)
+	/////==============
+	//var result0x float64 = math.Max(n3, n0)
+	//var result00 float64 = math.Max(n3, n1)
+	//var result10 float64 = math.Max(n3, n2)
+	//var result20 float64 = math.Max(n3, n)
+	//var result30 float64 = math.Max(n3, n4)
+	//var result40 float64 = math.Max(n3, n5)
+	//var result50 float64 = math.Max(n3, n6)
+	//var result60 float64 = math.Max(n3, n7)
+	//var result70 float64 = math.Max(n3, n8)
+	/////=======
+	//var result00 float64 = math.Max(n4, n0)
+	//var result000 float64 = math.Max(n4, n1)
+	//var result100 float64 = math.Max(n4, n2)
+	//var result200 float64 = math.Max(n4, n3)
+	//var result300 float64 = math.Max(n4, n)
+	//var result400 float64 = math.Max(n4, n5)
+	//var result500 float64 = math.Max(n4, n6)
+	//var result600 float64 = math.Max(n4, n7)
+	//var result700 float64 = math.Max(n4, n8)
+	/////==============
+	//var result000 float64 = math.Max(n5, n0)
+	//var result0000 float64 = math.Max(n5, n1)
+	//var result1000 float64 = math.Max(n5, n2)
+	//var result2000 float64 = math.Max(n5, n3)
+	//var result3000 float64 = math.Max(n5, n4)
+	//var result4000 float64 = math.Max(n5, n)
+	//var result5000 float64 = math.Max(n5, n6)
+	//var result6000 float64 = math.Max(n5, n7)
+	//var result7000 float64 = math.Max(n5, n8)
+	/////==========
+	//var result1x float64 = math.Max(n6, n0)
+	//var result01 float64 = math.Max(n6, n1)
+	//var result11 float64 = math.Max(n6, n2)
+	//var result21 float64 = math.Max(n6, n3)
+	//var result31 float64 = math.Max(n6, n4)
+	//var result41 float64 = math.Max(n6, n5)
+	//var result51 float64 = math.Max(n6, n)
+	//var result61 float64 = math.Max(n6, n7)
+	//var result71 float64 = math.Max(n6, n8)
+	/////========
+	//var result11 float64 = math.Max(n7, n0)
+	//var result011 float64 = math.Max(n7, n1)
+	//var result111 float64 = math.Max(n7, n2)
+	//var result211 float64 = math.Max(n7, n3)
+	//var result311 float64 = math.Max(n7, n4)
+	//var result411 float64 = math.Max(n7, n5)
+	//var result511 float64 = math.Max(n7, n6)
+	//var result611 float64 = math.Max(n7, n)
+	//var result711 float64 = math.Max(n7, n8)
+	/////==========
+	//var result111 float64 = math.Max(n8, n0)
+	//var result0111 float64 = math.Max(n8, n1)
+	//var result1111 float64 = math.Max(n8, n2)
+	//var result2111 float64 = math.Max(n8, n3)
+	//var result3111 float64 = math.Max(n8, n4)
+	//var result4111 float64 = math.Max(n8, n5)
+	//var result5111 float64 = math.Max(n8, n6)
+	//var result6111 float64 = math.Max(n8, n7)
+	//var result7111 float64 = math.Max(n8, n)
+
+	/// ====================
+
+	///	fmt.Printf("Result 1: %f", result)
+	///	var resut float64
+	// resut = Max(float64(n), float64(n0), float64(n1), float64(n2), float64(n3), float64(n4), float64(n5), float64(n6), float64(n7), float64(n8))
+	//var res_1 = math.MaxInt8(n, n0)
+	/////float64(n1), n2, n3, n4, n5, n6, n7, n8)
+	/// fmt.Printf("Result 1: %f", &MaxN)
+
+}
