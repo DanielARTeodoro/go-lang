b4ce3e4fa4880d1d6a6e90c0dbcc17082f6b69dd
Dani <51429507+DanielARTeodoro@users.noreply.github.com>
2022-11-26 17:22:44 -0300

    DEIVISON 13 - Cash - Go Lang
    DEIVISON 13 - Criar uma função que converta a moeda Real , para moeda dólar

--- List-sweet-chocolatery/DEIVISON/cash.go
+++ List-sweet-chocolatery/DEIVISON/cash.go
@@ -1,1 +1,27 @@
-Go lang;
+package main
+
+/// DEIVISON 13 - Criar uma função que converta a moeda Real , para moeda dólar
+
+import "fmt"
+
+var (
+	real  float64
+	dolar float64
+)
+
+func converterMoeda() {
+
+	fmt.Println("Digite o valor do Real: ")
+	fmt.Scan(&real)
+	fmt.Println("Digite o valor da cotação do Dolar para o Real: ")
+	fmt.Scan(&dolar)
+
+	fmt.Printf("O valor em Dolar é: %0.01f", real/dolar)
+
+}
+
+func main() {
+
+	converterMoeda()
+
+}
